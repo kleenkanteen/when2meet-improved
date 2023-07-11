1c45ed25c9f7ec657a4096cbade611b288435938
kleenkanteen <antpalmolive@gmail.com>
2023-07-10 22:47:43 -0400

    starter files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return (
+        <h1 className="text-3xl font-bold text-blue-600">
+      Install & Setup Vite + React + Typescript + Tailwind CSS 3
+      </h1>
+    )
+}
