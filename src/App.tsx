ee48b1aa23f9c210102e21e4054dd4a92b96e21b
kleenkanteen <antpalmolive@gmail.com>
2023-07-13 23:35:01 -0400

    get date calendar with multiselect working

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Home } from './pages/Home'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+// @ts-ignore
+import MultipleDatesPicker from '@ambiot/material-ui-multiple-dates-picker'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false)
 
   return (
-    <>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div>
+      <button onClick={() => setOpen(!open)}>
+        Select Dates
+      </button>
+      <MultipleDatesPicker
+        open={open}
+        selectedDates={[]}
+        onCancel={() => setOpen(false)}
+        onSubmit={(dates: Date[]) => console.log('selected dates', dates)}
+      />
+    </div>
       <h1 className="text-3xl font-bold text-blue-600">
       Install & Setup Vite + React + Typescript + Tailwind CSS 3
       </h1>
@@ -33,7 +49,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      </LocalizationProvider>
+
   )
 }
 
